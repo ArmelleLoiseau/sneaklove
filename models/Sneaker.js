1b6dbcbed863e2f8abc3b48d628b581a86483ec9
Alix Virot <flying.cat@mac.com>
2022-01-13 17:08:32 +0100

    model sneakers

---
+++ models/Sneaker.js
@@ -0,0 +1,22 @@
+const {model, Schema} = require("mongoose");
+
+const sneakerSchema = new Schema ({
+    name: String,
+    ref: String,
+    size: Number,
+    description: String,
+    price: Number,
+    category: {
+        type :String, 
+        enum :["men", "women", "kids"]
+    },
+    id_tags: {
+        type : Schema.Types.ObjectId,
+        ref : "tags"
+    }
+})
+
+const Sneaker = model("sneakers", sneakerSchema)
+
+module.exports = Sneaker;
+
